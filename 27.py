012b383e2270a2c864762eede930857ebefaac7b
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2020-05-17 19:50:52 +0300

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,1 +1,8 @@
-print("My name is Yoel")
+import psutil
+
+
+def free_disk_size():
+    total_size = psutil.disk_usage('C:').free
+    print('Total Size:', total_size, ' GB')
+
+free_disk_size()    
